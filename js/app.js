7fcf9a6e17483c98136f197f219a611a04570945
carmen <villalobo@hartford.edu>
2022-03-19 15:30:11 -0400

    modified:   css/style.css 	modified:   html/index.html 	modified:   js/app.js

--- js/app.js
+++ js/app.js
@@ -1,3 +1,60 @@
-window.onload = () => {
+/*window.onload = () => {
     alert(`JavaScript Works`);
 };
+*/
+var inrice = document.getElementById("inputtext");
+var outwater = document.getElementById("outputtext");
+document.getElementById("quantityrice").hidden = true;
+document.getElementById("quantitywater").hidden = true;
+white = false;
+let seleccionar = document.querySelector('select');
+seleccionar.addEventListener('change', establecerClima);
+
+
+function establecerClima() {
+  let eleccion = seleccionar.value;
+
+  if (eleccion === 'whiterice') {
+    document.getElementById("opciones").innerHTML = document.getElementById("whiterice").innerHTML;
+    white = true;
+    inrice.value = "";
+    outwater.value = "";
+  }
+  else {
+    document.getElementById("opciones").innerHTML = document.getElementById("sproutedcaliforniarice").innerHTML;
+    white = false;
+    inrice.value = "";
+    outwater.value = "";
+  }
+  document.getElementById("quantityrice").hidden = false;
+  document.getElementById("quantitywater").hidden = false;
+  document.getElementById("inputtext").focus();
+}
+function handle(e){
+ var key=e.keyCode || e.which;
+/*  if (key==38 || key==40){
+      calculationwater();
+  }*/
+  if (key== 38) {
+    if (inrice.value == "") inrice.value = 1;
+    else inrice.value++;
+    calculationwater();
+  }
+  else if (key== 40) {
+    if (inrice.value == "") inrice.value = 0;
+    else {
+      if (inrice.value != 0) inrice.value--;
+    }
+    calculationwater();
+  }
+
+}
+function calculationwater() {
+
+  if (white) {
+    outwater.value = inrice.value*2;
+  }
+  else {
+    outwater.value = (inrice.value*2)/1.25;
+  }
+}
