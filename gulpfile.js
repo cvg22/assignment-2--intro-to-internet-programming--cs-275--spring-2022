7df87550e695e7dc8985aeeb02d0958ce4747ee9
carmen <villalobo@hartford.edu>
2022-03-21 17:35:59 -0400

    Added tasks of validatehtml, lintjs, lintcs and transpilejs

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,77 @@
-const { src, dest, series } = require(`gulp`);
-const htmlValidator = require(`gulp-html`);
-const htmlCompressor = require(`gulp-htmlmin`);
+const { src, dest, series, watch } = require(`gulp`),
+    del = require(`del`),
+    htmlValidator = require(`gulp-html`),
+    htmlCompressor = require(`gulp-htmlmin`),
+    CSSLinter = require(`gulp-stylelint`),
+    jsLinter = require(`gulp-eslint`),
+    browserSync = require(`browser-sync`),
+    reload = browserSync.reload;
+
+let browserChoice = `default`;
 
 let validateHTML = () => {
-    return src([`html/*.html`, `html/**/*.html`]).pipe(htmlValidator());
+    return src(`html/*.html`).pipe(htmlValidator())
+    .pipe(dest(`temp`));
+};
+
+let lintCSS = () => {
+    return src(`css/*.css`)
+        .pipe(CSSLinter({
+            failAfterError: false,
+            reporters: [
+                {formatter: `string`, console: true}
+            ]
+        }))
+        .pipe(dest(`temp/css`));
+};
+
+let lintJS = () => {
+    return src(`js/*.js`)
+        .pipe(jsLinter())
+        .pipe(jsLinter.formatEach(`compact`));
+};
+
+let transpileJSForDev = () => {
+    return src(`js/*.js`)
+        .pipe(babel())
+        .pipe(dest(`temp/js`));
 };
 
 let compressHTML = () => {
-    return src([`html/*.html`,`html/**/*.html`])
+    return src(`html/*.html`)
         .pipe(htmlCompressor({collapseWhitespace: true}))
         .pipe(dest(`prod`));
 };
 
+
+let serve = () => {
+    browserSync({
+        notify: true,
+        reloadDelay: 100,
+        server: {
+            baseDir: `temp`
+        }
+    });
+
+    watch(`html/*.html`, validateHTML)
+        .on(`change`, reload);
+
+    watch(`css/*.css`, lintCSS)
+        .on(`change`, reload);
+
+    watch(`js/*.js`, lintJS)
+        .on(`change`, reload);
+};
+
 exports.validateHTML = validateHTML;
+exports.lintCSS = lintCSS;
+exports.lintJS = lintJS;
+exports.transpileJSForDev = transpileJSForDev;
 exports.compressHTML = compressHTML;
 exports.HTMLProcessing = series(validateHTML, compressHTML);
+exports.serve = series(
+    validateHTML,
+    lintCSS,
+    transpileJSForDev,
+    serve
+);
--- js/app.js
+++ js/app.js
@@ -2,56 +2,58 @@ window.onload = () => {
     alert(`JavaScript Works 1`);
 };
 
-var inrice = document.getElementById("inputtext");
-var outwater = document.getElementById("outputtext");
-document.getElementById("quantityrice").hidden = true;
-document.getElementById("quantitywater").hidden = true;
-white = false;
-let seleccionar = document.querySelector('select');
-seleccionar.addEventListener('change', ricechoice);
-
+var inrice = document.getElementById(`inputtext`);
+var outwater = document.getElementById(`outputtext`);
+document.getElementById(`quantityrice`).hidden = true;
+document.getElementById(`quantitywater`).hidden = true;
+let white = false;
+let seleccionar = document.querySelector(`select`);
+seleccionar.addEventListener(`change`, ricechoice);
+document.getElementById(`inputtext`).onkeydown = function() {handle(event);};
 
 function ricechoice() {
-  let eleccion = seleccionar.value;
+    let eleccion = seleccionar.value;
 
-  if (eleccion === 'whiterice') {
-    document.getElementById("opciones").innerHTML = document.getElementById("whiterice").innerHTML;
-    white = true;
-    inrice.value = "";
-    outwater.value = "";
-  }
-  else {
-    document.getElementById("opciones").innerHTML = document.getElementById("sproutedcaliforniarice").innerHTML;
-    white = false;
-    inrice.value = "";
-    outwater.value = "";
-  }
-  document.getElementById("quantityrice").hidden = false;
-  document.getElementById("quantitywater").hidden = false;
-  document.getElementById("inputtext").focus();
-}
-function handle(e){
- var key=e.keyCode || e.which;
-  if (key== 38) {
-    if (inrice.value == "") inrice.value = 1;
-    else inrice.value++;
-    calculationwater();
-  }
-  else if (key== 40) {
-    if (inrice.value == "") inrice.value = 0;
-    else {
-      if (inrice.value != 0) inrice.value--;
+    if (eleccion === `whiterice`) {
+        document.getElementById(`opciones`).innerHTML =
+        document.getElementById(`whiterice`).innerHTML;
+        white = true;
+        inrice.value = ``;
+        outwater.value = ``;
     }
-    calculationwater();
-  }
-
+    else {
+        document.getElementById(`opciones`).innerHTML =
+        document.getElementById(`sproutedcaliforniarice`).innerHTML;
+        white = false;
+        inrice.value = ``;
+        outwater.value = ``;
+    }
+    document.getElementById(`quantityrice`).hidden = false;
+    document.getElementById(`quantitywater`).hidden = false;
+    document.getElementById(`inputtext`).focus();
 }
-function calculationwater() {
 
-  if (white) {
-    outwater.value = inrice.value*2;
-  }
-  else {
-    outwater.value = (inrice.value*2)/1.25;
-  }
+function handle(e){
+    var key=e.keyCode || e.which;
+    if (key== 38) {
+        if (inrice.value == ``) inrice.value = 1;
+        else inrice.value++;
+        calculationwater();
+    }
+    else if (key== 40) {
+        if (inrice.value == ``) inrice.value = 0;
+        else {
+            if (inrice.value != 0) inrice.value--;
+        }
+        calculationwater();
+    }
+}
+
+function calculationwater() {
+    if (white) {
+        outwater.value = inrice.value*2;
+    }
+    else {
+        outwater.value = (inrice.value*2)/1.25;
+    }
 }
